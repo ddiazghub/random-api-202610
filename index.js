5126de8276b78838f2633c4bc4f4d3c102356c7b
David Diaz <daviddiaz@ballastlane.com>
2026-03-24 16:53:15 -0500

    add biome linter and formatter

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (re, res) => {
-  res.status(200).send("Hello World!!!");
+	res.status(200).send("Hello World!!!");
 });
 
 app.get("/random/int", (req, res) => {
-  res.status(200).send(crypto.randomInt(1000000));
+	res.status(200).send(crypto.randomInt(1000000));
 });
 
 app.get("/random/person", (req, res) => {
-  const person = {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    dateOfBirth: faker.date.birthdate(),
-  };
+	const person = {
+		firstName: faker.person.firstName(),
+		lastName: faker.person.lastName(),
+		dateOfBirth: faker.date.birthdate(),
+	};
 
-  res.status(200).send(person);
+	res.status(200).send(person);
 });
 
 app.listen(3000, () => console.log("Server running on port: ", 3000));
