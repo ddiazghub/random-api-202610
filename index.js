abf96e99610b565e0e14ecb79ac4a25f1d9fa84c
David Diaz <daviddiaz@ballastlane.com>
2026-03-24 17:07:48 -0500

    add pm2 process manager

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const express = require("express");
-const { faker } = require("@faker-js/faker");
+// const { faker } = require("@faker-js/faker");
 
 const app = express();
 
@@ -14,14 +14,14 @@ app.get("/random/int", (req, res) => {
 	res.status(200).send(crypto.randomInt(1000000));
 });
 
-app.get("/random/person", (req, res) => {
-	const person = {
-		firstName: faker.person.firstName(),
-		lastName: faker.person.lastName(),
-		dateOfBirth: faker.date.birthdate(),
-	};
-
-	res.status(200).send(person);
-});
+// app.get("/random/person", (req, res) => {
+// 	const person = {
+// 		firstName: faker.person.firstName(),
+// 		lastName: faker.person.lastName(),
+// 		dateOfBirth: faker.date.birthdate(),
+// 	};
+//
+// 	res.status(200).send(person);
+// });
 
 app.listen(3000, () => console.log("Server running on port: ", 3000));
